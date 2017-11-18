082c05dbc37b08d79b2ba2548d8b87cef47ee14e
styz <634909510@qq.com>
2017-11-18 10:14:46 +0800

    add tempalate

---
+++ src/com/huangh/demo/tem.java
@@ -0,0 +1,13 @@
+/**
+ * 测试模板
+ *
+ * @Author: huanghao
+ * @CREATE 2017-11-18 10:11
+ */
+package com.huangh.demo;
+
+public class tem {
+    public static void main(){
+        System.out.println("javatempalate");
+    }
+}
